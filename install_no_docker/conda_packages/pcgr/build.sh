4f834fa0bf2727918c31fc614cda07e8a16189aa
vladsaveliev <vladislav.sav@gmail.com>
2018-11-02 21:55:10 +1100

    pcgr recipe fix

--- install_no_docker/conda_packages/pcgr/build.sh
+++ install_no_docker/conda_packages/pcgr/build.sh
@@ -7,10 +7,12 @@ R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_gi
 #R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('Francescojm/CELLector', dependencies=FALSE)"
 
 # CONDA env vars: https://conda.io/docs/user-guide/tasks/build-packages/environment-variables.html
+mkdir -p ${PREFIX}/lib/R/library/pcgrr
+mkdir -p ${PREFIX}/bin
+mkdir -p ${SP_DIR}
 mv ${SRC_DIR}/src/pcgr/lib ${SP_DIR}/pcgr  # python modules
 mv ${SRC_DIR}/src/pcgr/*.py ${PREFIX}/bin/  # python scripts
-
-mkdir -p $PREFIX/lib/R/library/pcgrr && mv ${SRC_DIR}/src/R/pcgrr/* ${PREFIX}/lib/R/library/pcgrr/  # R modules
+mv ${SRC_DIR}/src/R/pcgrr/* ${PREFIX}/lib/R/library/pcgrr/  # R modules
 mv ${SRC_DIR}/src/R/*.R ${PREFIX}/bin/  # R scripts
 
 wget https://github.com/EBIvariation/vcf-validator/releases/download/v0.6/vcf_validator -O ${CONDA_PREFIX}/bin/vcf_validator
