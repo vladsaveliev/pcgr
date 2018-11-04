88d80ab332b41da800cacd2bb5a456837d1fa966
vladsaveliev <vladislav.sav@gmail.com>
2018-11-04 20:52:07 +1100

    pcgr recipe: copy pcgr.py script; add r-logging to env and shift to R3.5

--- install_no_docker/conda_packages/pcgr/build.sh
+++ install_no_docker/conda_packages/pcgr/build.sh
@@ -2,20 +2,20 @@
 # For the reference: CONDA env vars: https://conda.io/docs/user-guide/tasks/build-packages/environment-variables.html
 
 ### Few more R packages that are not on yet conda
-export TAR=/bin/tar  # to avoid "/bin/gtar: not found"
+#export TAR=/bin/tar  # to avoid "/bin/gtar: not found"
 # The "options(unzip = )" hack to address the install_github issue under conda https://github.com/r-lib/devtools/issues/1722
-R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('mjkallen/rlogging')"
 #R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('Francescojm/CELLector', dependencies=FALSE)"
 
 # Changing permissions to executables
 chmod +x ${SRC_DIR}/src/pcgr/*.py
+chmod +x ${SRC_DIR}/*.py
 chmod +x ${SRC_DIR}/src/*.R
 # Moving libraries and scripts
 mkdir -p ${PREFIX}/lib/R/library/pcgrr
 mkdir -p ${PREFIX}/bin
 mkdir -p ${SP_DIR}
 mv ${SRC_DIR}/src/pcgr/lib ${SP_DIR}/pcgr  # python modules
-mv ${SRC_DIR}/src/pcgr/*.py ${PREFIX}/bin/  # python scripts
+mv ${SRC_DIR}/src/pcgr/*.py ${SRC_DIR}/*.py ${PREFIX}/bin/  # python scripts
 mv ${SRC_DIR}/src/*.R ${PREFIX}/bin/  # R scripts
 # R modules:
 R -e "library(devtools); devtools::install('${SRC_DIR}/src/R/pcgrr', dependencies=FALSE)"
